1d4fb47dddcadc8cb7d97c289be8e92060afd1b4
Loreto Vaquero <loret.vaquero@gmail.com>
2019-05-28 14:13:15 +0200

    Scrapper

--- 2.Puppeteer/puppeteer.js
+++ 2.Puppeteer/puppeteer.js
@@ -20,11 +20,11 @@ const fs = require('fs');
 
 let url = 'https://www.idealista.com/alquiler-viviendas/madrid/retiro/pacifico/';
 
-const miFuncion = async() => {
+const getData = async() => {
     try {
         const browser = await puppeteer.launch({ headless: false });
         const context = await browser.createIncognitoBrowserContext();
-        const page = await browser.newPage();
+        const page = await context.newPage();
 
         await page.setRequestInterception(true);
 
@@ -41,37 +41,52 @@ const miFuncion = async() => {
         });
 
         await page.goto(url);
-
-        let data = await page.evaluate(() => {
-            let pisosData = [];
-            let pisos = document.querySelectorAll('#main-content > section > article');
-            console.log("LENGTH DE PISOS", pisos.length)
-            pisos.forEach((piso) => {
-                let pisoJson = {};
-                try {
-                    pisoJson.descripcion = piso.querySelector('#main-content > section > article > div > a').innerText;
-                    //pisoJson.id = piso.querySelector('#main-content > section > article > div > a').getAttribute('adid');; SOn uno misnistros y no puedo pillar el id
-                    pisoJson.id = piso.querySelector('#main-content > section > article > div > a').href.slice(35, -1);;
-                    pisoJson.precio = piso.querySelector('#main-content > section > article > div > div.row.price-row.clearfix > span').innerText;
-                    pisoJson.habitaciones = piso.querySelector('#main-content > section > article > div > span:nth-child(4)').innerText;
-                    pisoJson.metros = piso.querySelector('#main-content > section > article > div > span:nth-child(5)').innerText;
-                } catch (exception) {
-                    console.log(exception);
-                }
-                pisosData.push(pisoJson);
-
-            });
-            return pisosData;
+        let numViviendas = await page.evaluate(() => {
+            return parseInt(document.querySelector('#main > div > div.listing-top > nav > ul > li.current-level > span.breadcrumb-info').innerText);
         });
-        console.log(data);
-        //     await console.log(data);
-        //     await fs.writeFile('viviendas.json', JSON.stringfy(data), (err) => {
-        //         if (err) console.log(err);
-        //         console.log("Successfully Written to File.");
-        //     });
+        let pisos = await page.evaluate(() => {
+            let articles = [...document.querySelectorAll('#main-content > section > article')];
+            return articles.length;
+        });
+        let numPag = Math.ceil(numViviendas / pisos);
+        let pisosData;
+        let data;
+
+
+        for (let i = 2; i <= numPag; i++) {
+            (pisosData === undefined) ? pisosData = []: pisosData = [...data];
+            console.log(pisosData);
+            data = await page.evaluate((pisosData) => {
+                let pisos = [...document.querySelectorAll('#main-content > section > article')];
+                pisos.forEach((piso) => {
+                    let pisoJson = {};
+                    try {
+                        pisoJson.descripcion = piso.querySelector('#main-content > section > article > div > a').innerText;
+                        //pisoJson.id = piso.querySelector('#main-content > section > article > div > a').getAttribute('adid');; SOn uno misnistros y no puedo pillar el id
+                        pisoJson.id = piso.querySelector('#main-content > section > article > div > a').href.slice(35, -1);;
+                        pisoJson.precio = piso.querySelector('#main-content > section > article > div > div.row.price-row.clearfix > span').innerText;
+                        pisoJson.habitaciones = piso.querySelector('#main-content > section > article > div > span:nth-child(4)').innerText;
+                        pisoJson.metros = piso.querySelector('#main-content > section > article > div > span:nth-child(5)').innerText;
+                    } catch (exception) {
+                        console.log(exception);
+                    }
+                    pisosData.push(pisoJson);
+                });
+                return pisosData;
+            }, pisosData);
+            await page.waitFor(2 * 1000);
+            let pageUrl = url + `pagina-${i}.htm`;
+            await page.goto(pageUrl);
+            console.log(data);
+        }
+        return data;
+        fs.writeFile('viviendas.json', JSON.stringify(data), (err) => {
+            if (err) console.log(err);
+            console.log("Successfully Written to File.");
+        });
     } catch (error) {
         console.log(error);
-    }
-};
+    };
+}
 
-miFuncion();
+getData();
